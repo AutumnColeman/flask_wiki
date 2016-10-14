cc09e46a1c7a05d44a542023a9c05f022e87142e
Autumn Coleman <autumn.eve.coleman@gmail.com>
2016-10-14 14:48:01 -0400

    Wiki linkified

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect
+from wiki_linkify import wiki_linkify
+
 import pg
 
 db = pg.DB(dbname='Wiki')
@@ -16,12 +18,14 @@ def placeholder(page_name):
     if len(result_list) > 0:
         print result_list
         page_content = result_list[0].page_content
-        print page_content
-
+        linkify_content = wiki_linkify(page_content)
+        # print page_content
+        print linkify_content
         return render_template(
             'view.html',
             page_name = page_name,
-            page_content = query.namedresult()[0].page_content
+            # page_content = query.namedresult()[0].page_content,
+            linkify_content = linkify_content
         )
 
     else:
@@ -41,30 +45,29 @@ def edit_page(page_name):
 
 @app.route('/<page_name>/save', methods=['POST'])
 def save_edit(page_name):
-    # page_content = request.form.get('page_content')
-    # id = request.form.get('id')
-    # title = request.form.get('title')
+    page_content = request.form.get('page_content')
+    id = request.form.get('id')
+    title = request.form.get('title')
     # action = action.form.get('action')
-
+    # query = db.query("select id from page where title = '%s'" % page_name)
+    # id = query.namedresult()[0].id
     #
-    query = db.query("select id from page where title = '%s'" % page_name)
-    id = query.namedresult()[0].id
+    # print id
 
-    print id
-
-    db.update(
-        'page', {
-        'id': id,
-        'title': page_name,
-        'page_content': page_content
-    })
+    # db.update(
+    #     'page', {
+    #     'id': id,
+    #     'title': page_name,
+    #     'page_content': page_content
+    # })
 
     db.insert(
          'page',
          title=page_name,
          page_content=page_content
     )
-    return redirect('/<page_name>')
+    return redirect('/%s' % page_name)
+
     return "ok"
 if __name__ == '__main__':
     app.run(debug=True)
